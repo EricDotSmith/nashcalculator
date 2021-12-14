ca273a140df35bc852ab790075fcdcdde57861e9
erics112 <esmith11235@gmail.com>
2021-12-14 12:46:46 -0500

    add initial linting setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
+  const p = "we"
   return (
-    <div className="flex justify-center w-full bg-blue-100">
-      The Nash Calculator
+    <div className="w-screen h-screen bg-blue-100">
+      <div className="flex justify-center w-full bg-blue-200">
+        The Nash Calculator
+      </div>
+
+
+
+      <div  >sdf
+ 
+
+      </div>
     </div>
+    
   )
 }
 
