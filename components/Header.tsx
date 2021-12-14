91b0091f329a56d375b341e292a764ae2e5b0945
erics112 <esmith11235@gmail.com>
2021-12-14 15:12:33 -0500

    ...

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header: React.FC = () => {
 	return (
 		<div className="flex justify-center items-center w-full bg-blue-200 h-12">
-			The Nash Calculator
+			The Nash Calculator (In Progress)
 		</div>
 	);
 };
