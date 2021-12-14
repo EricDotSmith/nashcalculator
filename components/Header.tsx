39b186c91dd38799b0c61a0b15d656f4fd6685fa
erics112 <esmith11235@gmail.com>
2021-12-14 14:34:22 -0500

    some experimentation

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+	return (
+		<div className="flex justify-center items-center w-full bg-blue-200 h-12">
+			The Nash Calculator
+		</div>
+	);
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
+import CoreServicesCard from "../components/CoreServicesCard";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
 	return (
 		<div className="w-screen h-screen bg-blue-100">
-			<div className="flex justify-center w-full bg-blue-200">
-				The Nash Calculator
-			</div>
+			<Header />
+			<CoreServicesCard />
 		</div>
 	);
 };
