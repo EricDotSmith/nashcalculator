138f45b9209929a2e82904db49169c7a7b180520
erics112 <esmith11235@gmail.com>
2021-12-18 13:02:38 -0500

    remove old components

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,9 +1,6 @@
 import WelcomeMessage from "../components/WelcomeMessage";
 import ResultOverview from "../components/ResultOverview";
-import NashCoreServicesProportionalShareCard from "../components/NashCoreServicesProportionalShareCard";
-import NEXTokenAmountCard from "../components/NEXTokenAmountCard";
 import React from "react";
-import NashCoreServicesVolumeCard from "./NashCoreServicesVolumeCard";
 import NashCalculatorContextProvider from "./NashCalculatorContext";
 
 const Main: React.FC = () => {
@@ -12,13 +9,11 @@ const Main: React.FC = () => {
 			<div className="flex flex-col mx-auto p-4 space-y-4 max-w-screen-xl">
 				<WelcomeMessage />
 				<div className="flex flex-col space-y-4 w-full">
-					<NEXTokenAmountCard />
-					<NashCoreServicesVolumeCard />
+					{" "}
+					<ResultOverview />
 				</div>
 
 				<div className="flex flex-col sm:flex-row w-full space-y-4 sm:space-y-0 sm:space-x-4">
-					<NashCoreServicesProportionalShareCard />
-
 					<ResultOverview />
 				</div>
 			</div>
