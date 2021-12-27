018a53a1db25af2967f2d69b23a87ac6efdad02f
erics112 <esmith11235@gmail.com>
2021-12-27 10:26:30 -0500

    finalize layout of earnings card

--- components/Cards/EarningsCard.tsx
+++ components/Cards/EarningsCard.tsx
@@ -1,22 +1,27 @@
 import CurrencyInput from "react-currency-input-field";
 import CardContainer from "./CardContainer";
 import CardMessage from "./CardMessage";
+import Image from "next/image";
 
 const EarningsCard: React.FC = () => {
 	return (
 		<CardContainer title="Nash Earn" subheaderContent="Yearly TVL / APY" containerClasses="w-full sm:w-1/2">
 			<CardMessage>
-				Nash Earn is powered by a variety of DeFi protocols, with more to come. The defaults here aren&apos;t
-				representative of current data.
+				Nash Earn is powered by a variety of DeFi protocols, namely Aave and Anchor, with more to come. The defaults
+				here aren&apos;t representative of current data.
 			</CardMessage>
 			<div className="flex flex-col space-y-4">
 				<div className="flex flex-col space-y-4">
 					<div className="flex flex-col sm:flex-row space-x-0 sm:space-x-2 space-y-4 sm:space-y-0">
 						<div className="flex flex-row sm:flex-col items-center sm:items-start justify-between sm:justify-start w-full sm:w-3/5 min-w-0">
-							<div className="text-blue-50 text-base font-bold">Aave TVL</div>
+							<div className="text-blue-50 text-base space-x-1 font-bold flex items-center mb-0 sm:mb-1">
+								<Image src="/aave-logo.svg" alt="Aave Logo" width={30} height={30} title="Aave" />
+								<div>TVL</div>
+							</div>
+
 							<CurrencyInput
-								id="fiat-gateway-volume"
-								name="fiat-gateway-volume-input"
+								id="nash-earn-aave-tvl"
+								name="nash-earn-aave-tvl-input"
 								placeholder="Please enter a number"
 								value={100}
 								decimalsLimit={2}
@@ -31,14 +36,18 @@ const EarningsCard: React.FC = () => {
 							/>
 						</div>
 						<div className="flex flex-row sm:flex-col items-center sm:items-start justify-between sm:justify-start w-full sm:w-3/5 min-w-0">
-							<div className="text-blue-50 text-base font-bold">Aave APY</div>
+							<div className="text-blue-50 text-base space-x-1 font-bold flex items-center mb-0 sm:mb-1">
+								<Image src="/aave-logo.svg" alt="Aave Logo" width={30} height={30} title="Aave" />
+								<div>APY</div>
+							</div>
+
 							<CurrencyInput
-								id="fiat-gateway-volume"
-								name="fiat-gateway-volume-input"
+								id="nash-earn-aave-apy"
+								name="nash-earn-aave-apy-input"
 								placeholder="Please enter a number"
 								value={100}
 								decimalsLimit={2}
-								prefix="$"
+								suffix="%"
 								onValueChange={(value) => {
 									// setFiatGatewayPair((s) => ({
 									// 	...s,
@@ -54,10 +63,13 @@ const EarningsCard: React.FC = () => {
 				<div className="flex flex-col space-y-4">
 					<div className="flex flex-col sm:flex-row space-x-0 sm:space-x-2 space-y-4 sm:space-y-0">
 						<div className="flex flex-row sm:flex-col items-center sm:items-start justify-between sm:justify-start w-full sm:w-3/5 min-w-0">
-							<div className="text-blue-50 text-base font-bold">Anchor TVL</div>
+							<div className="text-blue-50 text-base space-x-1 font-bold flex items-center mb-0 sm:mb-1">
+								<Image src="/anchor-logo.svg" alt="Anchor Logo" width={30} height={30} title="Anchor" />
+								<div>TVL</div>
+							</div>
 							<CurrencyInput
-								id="fiat-gateway-volume"
-								name="fiat-gateway-volume-input"
+								id="nash-earn-anchor-tvl"
+								name="nash-earn-anchor-tvl-input"
 								placeholder="Please enter a number"
 								value={100}
 								decimalsLimit={2}
@@ -72,14 +84,17 @@ const EarningsCard: React.FC = () => {
 							/>
 						</div>
 						<div className="flex flex-row sm:flex-col items-center sm:items-start justify-between sm:justify-start w-full sm:w-3/5 min-w-0">
-							<div className="text-blue-50 text-base font-bold">Anchor APY</div>
+							<div className="text-blue-50 text-base space-x-1 font-bold flex items-center mb-0 sm:mb-1">
+								<Image src="/anchor-logo.svg" alt="Anchor Logo" width={30} height={30} title="Anchor" />
+								<div>APY</div>
+							</div>
 							<CurrencyInput
-								id="fiat-gateway-volume"
-								name="fiat-gateway-volume-input"
+								id="nash-earn-anchor-apy"
+								name="nash-earn-anchor-apy-input"
 								placeholder="Please enter a number"
 								value={100}
 								decimalsLimit={2}
-								prefix="$"
+								suffix="%"
 								onValueChange={(value) => {
 									// setFiatGatewayPair((s) => ({
 									// 	...s,
